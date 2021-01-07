ad3ea0b38ca28cd116e26059c20485985168bf43
Rahul Rudragoudar <rr83019@gmail.com>
2021-01-08 01:07:13 +0530

    Fix method to accept new line delimiter
    Signed-off-by: Rahul Rudragoudar <rr83019@gmail.com>

--- src/main/StringCalculator.java
+++ src/main/StringCalculator.java
@@ -6,17 +6,16 @@ public class StringCalculator {
 
 	public Integer Add(String str) {
 		if(str.isEmpty()) return 0;
-		else if(str.contains(",")){
-			String[] numbers = str.split(",");
+		else if(str.contains(",") || str.contains("\n")){
+			String[] numbers = str.split(",|\n");
 			Integer sum = Arrays.stream(numbers).mapToInt(this::toInt).sum();
 			return sum;
 		}
 		else return toInt(str);
 	}
-	
-	private int toInt(String strInt) {
+		
+	private int toInt(String strInt) throws NumberFormatException {
 		return Integer.parseInt(strInt);
 	}	
 	
-
 }
