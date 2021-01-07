5f41da171b1c1bbafdf35061943f0131da24475f
Rahul Rudragoudar <rr83019@gmail.com>
2021-01-08 02:15:14 +0530

    Fix method to accept custom delimiter
    Signed-off-by: Rahul Rudragoudar <rr83019@gmail.com>

--- src/main/StringCalculator.java
+++ src/main/StringCalculator.java
@@ -6,12 +6,21 @@ public class StringCalculator {
 
 	public Integer Add(String str) {
 		if(str.isEmpty()) return 0;
-		else if(str.contains(",") || str.contains("\n")){
-			String[] numbers = str.split(",|\n");
+		else {
+			String[] numbers = getDelimitedNumbers(str);
 			Integer sum = Arrays.stream(numbers).mapToInt(this::toInt).sum();
 			return sum;
 		}
-		else return toInt(str);
+	}
+	
+	private String[] getDelimitedNumbers(String str) {
+		if(str.startsWith("//")) {
+			String[] newStr = str.split("\n");
+			String delimiter = newStr[0].split("//")[1];
+			return newStr[1].split(delimiter);
+		} else {
+			return str.split(",|\n");
+		}
 	}
 		
 	private int toInt(String strInt) throws NumberFormatException {
