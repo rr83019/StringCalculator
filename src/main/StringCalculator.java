f2e51da9cccec2e095198c1cbead4cdae8fb080b
Rahul Rudragoudar <rr83019@gmail.com>
2021-01-07 22:37:48 +0530

    Fix method to check for 2 number args
    Signed-off-by: Rahul Rudragoudar <rr83019@gmail.com>

--- src/main/StringCalculator.java
+++ src/main/StringCalculator.java
@@ -4,6 +4,10 @@ public class StringCalculator {
 
 	public Integer Add(String str) {
 		if(str.isEmpty()) return 0;
+		else if(str.contains(",")){
+			String[] numbers = str.split(",");
+			return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+		}
 		else return Integer.parseInt(str);
 	}
 	
