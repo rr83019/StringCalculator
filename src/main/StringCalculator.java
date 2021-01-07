9d0d4aba602689a09bc7b2d932f0d7c0f24183e6
Rahul Rudragoudar <rr83019@gmail.com>
2021-01-08 00:18:29 +0530

    Fix method to check for multiple number args
    Signed-off-by: Rahul Rudragoudar <rr83019@gmail.com>

--- src/main/StringCalculator.java
+++ src/main/StringCalculator.java
@@ -1,20 +1,22 @@
 package main;
 
+import java.util.Arrays;
+
 public class StringCalculator {
 
 	public Integer Add(String str) {
 		if(str.isEmpty()) return 0;
 		else if(str.contains(",")){
 			String[] numbers = str.split(",");
-			return toInt(numbers[0]) + toInt(numbers[1]);
+			Integer sum = Arrays.stream(numbers).mapToInt(this::toInt).sum();
+			return sum;
 		}
 		else return toInt(str);
 	}
 	
-	private static int toInt(String strInt) {
+	private int toInt(String strInt) {
 		return Integer.parseInt(strInt);
-	}
-	
+	}	
 	
 
 }
